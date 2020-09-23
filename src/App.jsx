23660413c24ff62d4fbe8d6a2e69c937fc7f738a
MuhammadTalha123 <59221584+MuhammadTalha123@users.noreply.github.com>
2020-09-24 02:16:38 +0500

    updadte css and add button

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+
+
+const App = () => {
+
+    
+
+return(
+    <>
+        <div style = {{ backgroundColor: bg }}>
+            <button onClick = {bgChange} >Click Me</button>
+        </div>
+    </>
+);
+
+};
+
+export default App;
