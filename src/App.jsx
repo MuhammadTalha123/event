4ff72cffb86a3dd72d82e5a3b45489cdb947d5bf
MuhammadTalha123 <59221584+MuhammadTalha123@users.noreply.github.com>
2020-09-24 02:32:44 +0500

    add double click

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,24 @@ import React, { useState } from 'react';
 const App = () => {
 
     const purple = "#8e44ad"
- const [bg, setBg] = useState(purple);
+    const [bg, setBg] = useState(purple);
+    const [name, setName] = useState("Click Me");
+
     const bgChange = () => {
         let newBg = "#34495e"
         setBg(newBg);
+        setName("Ooch!! ");
+    };
+
+    const bgBack = () => {
+        setBg(purple);
+        setName("Ayyo!!"); 
     };
 
 return(
     <>
         <div style = {{ backgroundColor: bg }}>
-            <button onClick = {bgChange} >Click Me</button>
+            <button onClick = {bgChange}  onDoubleClick = {bgBack} > {name} </button>
         </div>
     </>
 );
